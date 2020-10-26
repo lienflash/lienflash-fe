5b646c705528e5935ac6637449b490cd2607f181
Stephanie Norton <steph.jane.norton@gmail.com>
2020-10-26 16:08:03 -0600

    Merge conflicts

---
+++ src/containers/JobForm/JobForm.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import './JobForm.scss'
+
+const JobForm = () => {
+
+  return (
+    <form className='form'>
+      <h1>New Job</h1>
+    </form>
+  )
+}
+
+export default JobForm

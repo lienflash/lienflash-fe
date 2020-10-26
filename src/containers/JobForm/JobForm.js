7501676481a92a00dc3092b59fd029d6a904c75f
Stephanie Norton <steph.jane.norton@gmail.com>
2020-10-26 08:40:10 -0600

    Render the two form components inside main form container

--- src/containers/JobForm/JobForm.js
+++ src/containers/JobForm/JobForm.js
@@ -1,11 +1,36 @@
 import React, { useState } from 'react'
+import JobFormStepOne from '../../components/JobFormStepOne.js/JobFormStepOne'
+import JobFormStepTwo from '../../components/JobFormStepTwo.js/JobFormStepTwo'
 import './JobForm.scss'
 
 const JobForm = () => {
+  const [input, setInput] = useState({})
+  const [currentStep, updateStep] = useState(1)
+
+  const handleInputChange = (e) => setInput({
+    ...input,
+    [e.currentTarget.name]: e.currentTarget.value
+  })
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const newJob = input
+    console.log(newJob)
+  }
 
   return (
     <form className='form'>
-      <h1>New Job</h1>
+      <h1>Add Job</h1>
+      <p>Step {currentStep} </p>
+      <JobFormStepOne
+        handleInputChange={handleInputChange}
+        currentStep={currentStep}
+        updateStep={updateStep}
+      />
+      <JobFormStepTwo
+        handleInputChange={handleInputChange}
+        handleSubmit={handleSubmit}
+      />
     </form>
   )
 }

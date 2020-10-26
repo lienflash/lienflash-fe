80a128948019ec60d664d49221d4ecc7a9fc1f24
Stephanie Norton <steph.jane.norton@gmail.com>
2020-10-26 14:02:04 -0600

    Write and pass tests for storing form input values

--- src/components/JobFormStepOne/JobFormStepOne.test.js
+++ src/components/JobFormStepOne/JobFormStepOne.test.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import '@testing-library/jest-dom'
-import { screen, render } from '@testing-library/react'
+import { screen, render, fireEvent } from '@testing-library/react'
 import JobFormStepOne from './JobFormStepOne'
 
 describe('JobFormStepOne component', () => {
-  it('Should render step two of the form', () => {
 
+  beforeEach(() => {
     render(
-      <JobFormStepOne 
+      <JobFormStepOne
         handleInputChange={jest.fn()}
-        currentStep={1}/>
+        currentStep={1} />
     )
+  })
+
+  it('Should render step two of the form', () => {
 
     const heading = screen.getByRole( 'heading', { name: 'Job Site Details' })
     const jobType = screen.getByText('Job Type:', { exact: false })
@@ -39,5 +42,45 @@ describe('JobFormStepOne component', () => {
     expect(laborCosts).toBeInTheDocument()
     expect(materialCosts).toBeInTheDocument()
     expect(totalCosts).toBeInTheDocument()
+  })
+
+  it('should store the form values that are inputted by user', () => {
+
+    const jobTypeInput = screen.getByLabelText('job-type')
+    const jobSiteNameInput = screen.getByLabelText('job-site-name')
+    const siteAddressInput = screen.getByLabelText('job-site-address')
+    const siteAddress2Input = screen.getByLabelText('job-site-address-line-2')
+    const cityInput = screen.getByLabelText('job-site-city')
+    const stateInput = screen.getByLabelText('job-site-state')
+    const zipCodeInput = screen.getByLabelText('job-site-zip-code')
+    // const dateInput = screen.getByLabelText('job-completion-date')
+    const laborCostsInput = screen.getByLabelText('labor-costs-due')
+    const materialCostsInput = screen.getByLabelText('material-costs-due')
+    const totalCostsInput = screen.getByLabelText('total-costs-due')
+    
+    fireEvent.change(jobTypeInput, { target: { value: 'Labor' } })
+    fireEvent.change(jobSiteNameInput, { target: { value: 'Burt\'s Warehouse' } })
+    fireEvent.change(siteAddressInput, { target: { value: '1777 Myrtle Drive' } })
+    fireEvent.change(siteAddress2Input, { target: { value: 'Apt 1209' } })
+    fireEvent.change(cityInput, { target: { value: 'Denver' } })
+    fireEvent.change(stateInput, { target: { value: 'CO' } })
+    fireEvent.change(zipCodeInput , { target: { value: '80240' } })
+    // fireEvent.change(dateInput, { target: { value: '10/25/2020' } })
+    fireEvent.change(laborCostsInput, { target: { value: '10000' } })
+    fireEvent.change(jobSiteNameInput, { target: { value: 'Burt\'s Warehouse' } })
+    fireEvent.change(materialCostsInput, { target: { value: '0' } })
+    fireEvent.change(totalCostsInput, { target: { value: '10000' } })
+
+    expect(jobTypeInput.value).toBe('Labor')
+    expect(jobSiteNameInput.value).toBe('Burt\'s Warehouse')
+    expect(siteAddressInput.value).toBe('1777 Myrtle Drive')
+    expect(siteAddress2Input.value).toBe('Apt 1209')
+    expect(cityInput.value).toBe('Denver')
+    expect(stateInput.value).toBe('CO')
+    expect(zipCodeInput.value).toBe('80240')
+    // expect(dateInput.value).toBe('10/25/2020')
+    expect(laborCostsInput.value).toBe('10000')
+    expect(materialCostsInput.value).toBe('0')
+    expect(totalCostsInput.value).toBe('10000')
   })
 })
--- src/components/JobFormStepTwo/JobFormStepTwo.test.js
+++ src/components/JobFormStepTwo/JobFormStepTwo.test.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import '@testing-library/jest-dom'
-import { screen, render } from '@testing-library/react'
+import { screen, render, fireEvent } from '@testing-library/react'
 import JobFormStepTwo from './JobFormStepTwo'
 
 describe('JobFormStepTwo component', () => {
-  it('Should render step one of the form', () => {
+  beforeEach(() => {
 
     render(
-      <JobFormStepTwo 
+      <JobFormStepTwo
         handleInputChange={jest.fn()}
         currentStep={2}
       />
     )
+  })
+
+  it('Should render step one of the form', () => {
 
     const heading = screen.getByRole('heading', { name: 'Client / Company Details' })
     const companyName = screen.getByText('Client / Company Name:')
@@ -32,5 +35,38 @@ describe('JobFormStepTwo component', () => {
     expect(state).toBeInTheDocument()
     expect(email).toBeInTheDocument()
     expect(phone).toBeInTheDocument()
+  })
+
+  it('should store the form values that are inputted by user', () => {
+
+    const clientNameInput = screen.getByLabelText('client-company-name')
+    const clientContactNameInput = screen.getByLabelText('client-contact-name')
+    const clientBusinessAddress = screen.getByLabelText('client-address')
+    const clientAddressLine2 = screen.getByLabelText('client-address-line-2')
+    const cityInput = screen.getByLabelText('client-address-city')
+    const stateInput = screen.getByLabelText('client-address-state')
+    const zipCodeInput = screen.getByLabelText('client-address-zipcode')
+    const emailInput = screen.getByLabelText('client-email')
+    const phoneInput = screen.getByLabelText('client-phone')
+
+    fireEvent.change(clientNameInput, { target: { value: 'Burts' } })
+    fireEvent.change(clientContactNameInput, { target: { value: 'Burt Rumble' } })
+    fireEvent.change(clientBusinessAddress, { target: { value: '100 Myrtle Drive' } })
+    fireEvent.change(clientAddressLine2, { target: { value: '' } })
+    fireEvent.change(cityInput, { target: { value: 'Denver' } })
+    fireEvent.change(stateInput, { target: { value: 'CO' } })
+    fireEvent.change(zipCodeInput, { target: { value: '80242' } })
+    fireEvent.change(emailInput , { target: { value: 'bm@mail.com' } })
+    fireEvent.change(phoneInput, { target: { value: '888 888 7777' } })
+    
+    expect(clientNameInput.value).toBe('Burts')
+    expect(clientContactNameInput.value).toBe('Burt Rumble')
+    expect(clientBusinessAddress.value).toBe('100 Myrtle Drive')
+    expect(clientAddressLine2.value).toBe('')
+    expect(cityInput.value).toBe('Denver')
+    expect(stateInput.value).toBe('CO')
+    expect(zipCodeInput.value).toBe('80242')
+    expect(emailInput.value).toBe('bm@mail.com')
+    expect(phoneInput.value).toBe('888 888 7777')
   })
 })

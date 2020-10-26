273bc9caee7a6a8f8a13ef55b2e87e40ae2aa45e
Stephanie Norton <steph.jane.norton@gmail.com>
2020-10-26 11:31:19 -0600

    Pass test for rendering correct heading

--- src/components/JobFormStepOne/JobFormStepOne.test.js
+++ src/components/JobFormStepOne/JobFormStepOne.test.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import '@testing-library/jest-dom'
-import { screen, render, fireEvent } from '@testing-library/react'
+import { screen, render } from '@testing-library/react'
 import JobFormStepOne from './JobFormStepOne'
 
 describe('JobFormStepOne component', () => {
-  it('Should render the Add New Job form', () => {
+  it('Should render the step one of the form', () => {
 
     render(
-      <JobFormStepOne />
+      <JobFormStepOne 
+        handleInputChange={jest.fn()}
+        currentStep={1}
+      />
     )
 
-    const heading = screen.getByText('heading', { name: 'Company Details' })
+    const heading = screen.getByRole('heading', { name: 'Company Details' })
     expect(heading).toBeInTheDocument()
   })
 })

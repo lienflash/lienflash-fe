813f0bf6cdd645c56d3823a09fabc78e9bf21ae3
Taryn Martin <taryn.martin919@gmail.com>
2020-10-28 19:59:16 -0600

    Complete job details functionality

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import jobsReducer from './jobsReducer.js';
+import jobInfoReducer from './jobInfoReducer.js'
 
 const rootReducer = combineReducers({
   allJobs: jobsReducer,
+  jobInfo: jobInfoReducer
 });
 
 export default rootReducer;

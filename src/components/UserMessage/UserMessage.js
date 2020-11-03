0faa2c0c434b07a54736cc16d3d428c498f6066e
Stephanie Norton <steph.jane.norton@gmail.com>
2020-11-02 19:47:36 -0700

    Add conditional to remove job button and edit message content

--- src/components/UserMessage/UserMessage.js
+++ src/components/UserMessage/UserMessage.js
@@ -5,29 +5,16 @@ import 'reactjs-popup/dist/index.css';
 const UserMessage = ({ dateDifference, status, handleClick }) => {
 
   return (
-    // {/* buttons depending on where in the process they are */ }
+    //  {/* buttons depending on where in the process they are */ }
     <div>
       {(dateDifference > 10 || status === 'NOI Eligible') &&
         <Popup trigger={<button className='btn-submit'>Submit NOI</button>} position="top left">
           {close => (
             <section className="popup-msg">
-              Please confirm if you would like to purchase ... for $45.00
+              Please confirm if you would like to submit a NOI for this job. 
+              <br /> <br />
+              Your account will be charged $0.00.
               <button className="close" onClick={close}>Close
-              &times;
-              </button>
-              <button className="confirm" onClick={() => handleClick(0)}>Confirm
-              </button>
-            </section>
-          )}
-        </Popup>
-      }
-   
-      {status === 'NOI filed' &&
-        <Popup trigger={<button className='btn-submit'>Submit Lien</button>} position="top left">
-          {close => (
-            <section className="popup-msg">
-              Please confirm if you would like to purchase ... for $45.00
-                <button className="close" onClick={close}>Close
               &times;
               </button>
               <button className="confirm" onClick={() => handleClick(2)}>Confirm
@@ -36,34 +23,55 @@ const UserMessage = ({ dateDifference, status, handleClick }) => {
           )}
         </Popup>
       }
+      
+      {(status === 'good standing' || status === 'NOI Eligible') &&
+        <Popup trigger={<button className='btn-submit'>Remove Job</button>} position="top left">
+          {close => (
+            <section className="popup-msg">
+              Are you sure you want to remove this job?
+              <button className="close" onClick={close}>Close
+              &times;
+              </button>
+              <button className="confirm" onClick={() => handleClick(5)}>Confirm
+            </button>
+            </section>
+           )}
+        </Popup>
+        }
+      
+      {status === 'NOI filed' &&
+        <Popup trigger={<button className='btn-submit'>Submit Lien</button>} position="top left">
+          {close => (
+            <section className="popup-msg">
+              Please confirm if you would like to submit a NOI for this job.
+              <br /><br />
+              Your account will be charged $0.00.
+              <button className="close" onClick={close}>Close
+              &times;
+              </button>
+              <button className="confirm" onClick={() => handleClick(3)}>Confirm
+              </button>
+            </section>
+          )}
+        </Popup>
+      }
 
       {status === 'Lien Filed' &&
         <Popup trigger={<button className='btn-submit'>Submit Release of Lien</button>} position="top left">
-        {close => (
-          <section className="popup-msg">
-            Please confirm if you would like to release lien... for $45.00
-            <button className="close" onClick={close}>Close
-            &times;
-            </button>
-            <button className="confirm" onClick={() => handleClick(3)}>Confirm
-            </button>
-          </section>
-        )}
-      </Popup>
+          {close => (
+            <section className="popup-msg">
+              Please confirm if you would like to release the lien for this job.
+              <br /><br />
+              Your account will be charged $0.00.
+              <button className="close" onClick={close}>Close
+              &times;
+              </button>
+              <button className="confirm" onClick={() => handleClick(5)}>Confirm
+              </button>
+            </section>
+          )}
+        </Popup>
       }
-
-      <Popup trigger={<button className='btn-submit'>Remove Job</button>} position="top left">
-        {close => (
-          <section className="popup-msg">
-            Are you sure you want to remove this job?
-            <button className="close" onClick={close}>Close
-            &times;
-            </button>
-            <button className="confirm" onClick={() => handleClick(4)}>Confirm
-            </button>
-          </section>
-        )}
-      </Popup>
     </div>
   )
 };

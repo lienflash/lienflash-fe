1284d7e23216ac84d8cd9c6e5a1f3e09d78da1e1
Stephanie Norton <steph.jane.norton@gmail.com>
2020-11-18 08:18:19 -0700

    Set token in localStorage on login

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -24,6 +24,7 @@ function Login() {
       postLogin(input)
         .then(response => {
           dispatch(setUser(response.data))
+          localStorage.setItem('token', response.data.attributes.token)
           history.push('/homepage')
         })
         .catch(error => {

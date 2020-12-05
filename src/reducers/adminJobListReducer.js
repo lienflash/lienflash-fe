8afeb1040d9693e2e6c70d10439936e6cb396b80
Stephanie Norton <steph.jane.norton@gmail.com>
2020-12-05 11:49:40 -0700

    Write reducer test for adminJobList

--- src/reducers/adminJobListReducer.js
+++ src/reducers/adminJobListReducer.js
@@ -1,12 +1,11 @@
 const adminJobListReducer = ( state=[], action) => {
-  console.log(action.type)
   switch(action.type) {
     case 'ADMIN_JOB_LIST':
-    return action.jobs.map(job => {
-      let attributes = job.attributes
-      attributes['id'] = job.id
-      return attributes
-    })
+      return action.jobs.map(job => {
+        let attributes = job.attributes
+        attributes['id'] = job.id
+        return attributes
+      })
     default: 
       return state
   }
